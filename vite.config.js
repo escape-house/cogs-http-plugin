b3ca5a0cc4ae3af3e6038c4d4c93644cd1b9bef3
mStenz <michael.stenz2006@gmail.com>
2026-04-28 13:57:36 +0200

    [GH Actions] add releas action

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: "./",
   build: {
-    outDir: "./build/dev.stenz.http-plugin",
+    outDir: "./build/dev.stenz.http",
     emptyOutDir: true,
   },
 });
